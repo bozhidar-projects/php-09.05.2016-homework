d529d1d90891db657b767f1de608b7c8133767e2
Petar Stoyanov <petar.stoyanov@gmail.com>
2016-07-02 20:12:03 +0300

    Homework8

---
+++ petar-stoyanov/homework8/admin.php
@@ -0,0 +1,10 @@
+<?php
+	$a1 = '<li><a href="admin.php" target="_self">Admin page</a></li>';
+	$a2 = '<li><a href="articles.php" target="_self">Articles</a></li>';
+	$a3 = '<li><a href="mails.php" target="_self">Messages</a></li>';
+	$a4 = '<li><a href="members.php" target="_self">Members</a></li>';
+	include("header.php");
+	include("left-sidebar.php");
+	include("content.php");
+	include("footer.php");
+?>
---
+++ petar-stoyanov/homework8/left-sidebar.php
@@ -0,0 +1,14 @@
+<aside id="site-navigation">
+	<header>
+		<h3>Sections:</h3>
+	</header>
+	<nav>
+		<ul><?php
+			echo $a1;
+			echo $a2;
+			echo $a3;
+			echo $a4;
+			?>
+		</ul>
+	</nav>
+</aside>
